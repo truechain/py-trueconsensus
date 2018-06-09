66dc8c17e2af4d0e3a56f1378cb35e55180ac365
Archit Sharma <archit@pm.me>
2018-06-10 02:06:12 +0530

    remove async/

--- trueconsensus/fastchain/pbft-py/async/make_keys.py
+++
@@ -1,8 +0,0 @@
-import sig
-import sys
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "USAGE"
-        sys.exit(0)
-    sig.write_new_keys(int(sys.argv[1]))
--- trueconsensus/fastchain/pbft-py/async/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-gnome-terminal -e "./server.py 0 1000"
-gnome-terminal -e "./server.py 1 1000"
-gnome-terminal -e "./server.py 2 1000"
-gnome-terminal -e "./server.py 3 1000"
