8fe52259a91da0ce4d3cc6ae0e2b57ce48d5b0ae
Archit Sharma <archit@pm.me>
2018-06-23 01:30:14 -0700

    replace sockets with grpc interface; This in turn, replaces struct buff packs with grpc blobs

--- trueconsensus/proto/request_pb2.py
+++ trueconsensus/proto/request_pb2.py
@@ -18,8 +18,8 @@ _sym_db = _symbol_database.Default()
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='trueconsensus/proto/request.proto',
   package='request',
-  syntax='proto2',
-  serialized_pb=_b('\n!trueconsensus/proto/request.proto\x12\x07request\"\xb7\x01\n\x07Request\x12%\n\x05inner\x18\x01 \x02(\x0b\x32\x16.request.Request.Inner\x12\x0b\n\x03\x64ig\x18\x02 \x02(\x0c\x12\x0b\n\x03sig\x18\x03 \x02(\x0c\x12\r\n\x05outer\x18\x04 \x01(\x0c\x1a\\\n\x05Inner\x12\n\n\x02id\x18\x01 \x02(\x05\x12\x0b\n\x03seq\x18\x02 \x02(\x05\x12\x0c\n\x04view\x18\x03 \x02(\x05\x12\x0c\n\x04type\x18\x04 \x02(\t\x12\x0b\n\x03msg\x18\x05 \x02(\x0c\x12\x11\n\ttimestamp\x18\x06 \x01(\x05\"(\n\x07History\x12\x1d\n\x03req\x18\x01 \x03(\x0b\x32\x10.request.Request')
+  syntax='proto3',
+  serialized_pb=_b('\n!trueconsensus/proto/request.proto\x12\x07request\"\xb7\x01\n\x07Request\x12%\n\x05inner\x18\x01 \x01(\x0b\x32\x16.request.Request.Inner\x12\x0b\n\x03\x64ig\x18\x02 \x01(\x0c\x12\x0b\n\x03sig\x18\x03 \x01(\x0c\x12\r\n\x05outer\x18\x04 \x01(\x0c\x1a\\\n\x05Inner\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0b\n\x03seq\x18\x02 \x01(\x05\x12\x0c\n\x04view\x18\x03 \x01(\x05\x12\x0c\n\x04type\x18\x04 \x01(\t\x12\x0b\n\x03msg\x18\x05 \x01(\x0c\x12\x11\n\ttimestamp\x18\x06 \x01(\x05\"(\n\x07History\x12\x1d\n\x03req\x18\x01 \x03(\x0b\x32\x10.request.Requestb\x06proto3')
 )
 
 
@@ -34,35 +34,35 @@ _REQUEST_INNER = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='id', full_name='request.Request.Inner.id', index=0,
-      number=1, type=5, cpp_type=1, label=2,
+      number=1, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
       name='seq', full_name='request.Request.Inner.seq', index=1,
-      number=2, type=5, cpp_type=1, label=2,
+      number=2, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
       name='view', full_name='request.Request.Inner.view', index=2,
-      number=3, type=5, cpp_type=1, label=2,
+      number=3, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
       name='type', full_name='request.Request.Inner.type', index=3,
-      number=4, type=9, cpp_type=9, label=2,
+      number=4, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
       name='msg', full_name='request.Request.Inner.msg', index=4,
-      number=5, type=12, cpp_type=9, label=2,
+      number=5, type=12, cpp_type=9, label=1,
       has_default_value=False, default_value=_b(""),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -82,7 +82,7 @@ _REQUEST_INNER = _descriptor.Descriptor(
   ],
   options=None,
   is_extendable=False,
-  syntax='proto2',
+  syntax='proto3',
   extension_ranges=[],
   oneofs=[
   ],
@@ -99,21 +99,21 @@ _REQUEST = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='inner', full_name='request.Request.inner', index=0,
-      number=1, type=11, cpp_type=10, label=2,
+      number=1, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
       name='dig', full_name='request.Request.dig', index=1,
-      number=2, type=12, cpp_type=9, label=2,
+      number=2, type=12, cpp_type=9, label=1,
       has_default_value=False, default_value=_b(""),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
       name='sig', full_name='request.Request.sig', index=2,
-      number=3, type=12, cpp_type=9, label=2,
+      number=3, type=12, cpp_type=9, label=1,
       has_default_value=False, default_value=_b(""),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -133,7 +133,7 @@ _REQUEST = _descriptor.Descriptor(
   ],
   options=None,
   is_extendable=False,
-  syntax='proto2',
+  syntax='proto3',
   extension_ranges=[],
   oneofs=[
   ],
@@ -164,7 +164,7 @@ _HISTORY = _descriptor.Descriptor(
   ],
   options=None,
   is_extendable=False,
-  syntax='proto2',
+  syntax='proto3',
   extension_ranges=[],
   oneofs=[
   ],
